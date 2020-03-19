9c3fb99096ebd1d2d01465e991c1c9f2e96fbf0d
Julian Pufler <puf17640@spengergasse.at>
2020-03-19 20:06:54 +0100

    hopefully working now

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ if(process.env.SSL_KEY_PATH && process.env.SSL_CERT_PATH)
   https.createServer({ key: fs.readFileSync(path.resolve(process.env.SSL_KEY_PATH), 'utf8'), cert: fs.readFileSync(path.resolve(process.env.SSL_CERT_PATH), 'utf8')}, app).listen(process.env.HTTPS_PORT, () => console.log(`listening on port ${process.env.HTTPS_PORT}`))
 
 
-//cron.schedule("0 15 20 * * *", ()=>{
+cron.schedule("0 15 20 * * *", ()=>{
   console.log(new Date())
   getCountries().then(countries => {
     Promise.all(countries.map(country => new Promise((resolve, reject) => {
@@ -160,27 +160,6 @@ if(process.env.SSL_KEY_PATH && process.env.SSL_CERT_PATH)
           resolve(obj)
         }))).then(data => {
           for(var obj of data){
-            console.log({ 
-              country: obj.country,
-              firstConfirmed: Math.floor(Math.abs(moment.duration(moment(obj.firstConfirmed).diff(moment())).asDays())) || 0,
-              firstConfirmedDate: isNaN(new Date(obj.firstConfirmed).getTime()) ? "N/A" : new Date(obj.firstConfirmed).toLocaleDateString(),
-              firstRecovery: Math.floor(Math.abs(moment.duration(moment(obj.firstRecovery).diff(moment())).asDays())) || 0,
-              firstRecoveryDate: isNaN(new Date(obj.firstRecovery).getTime()) ? "N/A" : new Date(obj.firstRecovery).toLocaleDateString(),
-              firstDeath: Math.floor(Math.abs(moment.duration(moment(obj.firstDeath).diff(moment())).asDays())) || 0,
-              firstDeathDate: isNaN(new Date(obj.firstDeath).getTime()) ? "N/A" : new Date(obj.firstDeath).toLocaleDateString(),
-              totalConfirmed: obj.totalConfirmed,
-              totalRecoveries: obj.totalRecoveries,
-              totalDeaths: obj.totalDeaths,
-              increaseConfirmed: obj.increaseConfirmed,
-              increaseRecoveries: obj.increaseRecoveries,
-              increaseDeaths: obj.increaseDeaths,
-              increaseConfirmedNum: obj.increaseConfirmedNum,
-              increaseRecoveriesNum: obj.increaseRecoveriesNum,
-              increaseDeathsNum: obj.increaseDeathsNum,
-              users: obj.users,
-              countrySlug: obj.country.toLowerCase().replace(/ /g, '-')
-            })
-            continue;
             mailer.send({
               personalizations: obj.users.map(u => ({
                 to: [{email:u.email}], 
@@ -215,4 +194,4 @@ if(process.env.SSL_KEY_PATH && process.env.SSL_CERT_PATH)
       }).catch((e) => console.log("err: "+e))
     }).catch(console.log)
   })
-//})
+})
