ab8a43bfd555b2fe943fbb0de89c199befa34a33
Julian Pufler <puf17640@spengergasse.at>
2020-03-27 18:07:56 +0100

    whoooooooopsie..........

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ if(isProduction)
       res.redirect(`https://${req.hostname}${req.path}`)
   })
 
-app.get('/', (req, res, next) => res.render("index", { unregistered: req.session.unregistered, user: req.session.user, error: req.session.err, data: getCountries()})).catch((err)=>res.status(500).send(err))
+app.get('/', (req, res, next) => res.render("index", { unregistered: req.session.unregistered, user: req.session.user, error: req.session.err, data: getCountries()}))
 
 app.use((req, res, next) => {
   delete req.session.user
